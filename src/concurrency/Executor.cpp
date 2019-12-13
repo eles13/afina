279b02fc60acd39b511de41862e21afa5f2445ea
Елисей <48096405+eles13@users.noreply.github.com>
2019-12-13 14:01:08 +0300

    hw3

--- src/concurrency/Executor.cpp
+++ src/concurrency/Executor.cpp
@@ -1,63 +1,72 @@
 #include <afina/concurrency/Executor.h>
-#include <algorithm>
+#include <iostream>
 namespace Afina {
 namespace Concurrency {
 void perform(Executor *executor) {
-    while (executor->state == Executor::State::kRun) {
-        std::function<void()> task;
-        {
-            std::unique_lock<std::mutex> lock(executor->mutex);
-            auto time = std::chrono::system_clock::now() + std::chrono::milliseconds(executor->idle_time);
-            while ((executor->tasks.empty()) && (executor->state == Executor::State::kRun)) {
-                executor->freeth++;
-                if (executor->empty_condition.wait_until(lock, time) == std::cv_status::timeout) {
-                    if (executor->threads.size() > executor->low_watermark) {
-                        auto it = executor->threads.find(std::this_thread::get_id());
-                        if (it != executor->threads.end()) {
-                        executor->threads.erase(it);
-                        return;
-                    }
-                    } else
-                        executor->empty_condition.wait(lock);
-                }
-                executor->freeth--;
-            }
-            if (executor->tasks.empty())
-                continue;
-            task = executor->tasks.front();
-            executor->tasks.pop_front();
+  while (executor->state == Executor::State::kRun) {
+    std::function<void()> task;
+
+    {
+      std::unique_lock<std::mutex> lock(executor->mutex);
+      auto time = std::chrono::system_clock::now() +
+                  std::chrono::milliseconds(executor->idle_time);
+      while ((executor->tasks.empty()) &&
+             (executor->state == Executor::State::kRun)) {
+        if (executor->empty_condition.wait_until(lock, time) ==
+                std::cv_status::timeout &&
+            executor->threads + executor->freeth > executor->low_watermark) {
+          return;
+        } else {
+          executor->empty_condition.wait(lock);
         }
-        task();
+      }
+      if (executor->tasks.empty())
+        continue;
+      task = executor->tasks.front();
+      executor->tasks.pop_front();
+      executor->threads++;
+      executor->freeth--;
+    }
+    try {
+      task();
+    } catch (...) {
+      std::terminate();
     }
     {
-        std::unique_lock<std::mutex> lock(executor->mutex);
-        auto it =executor->threads.find(std::this_thread::get_id());
-        executor->threads.erase(it);
-        if (executor->tasks.empty())
-            executor->stop.notify_all();
+      std::unique_lock<std::mutex> lock(executor->mutex);
+      executor->freeth++;
+      executor->threads--;
+      if (executor->state == Executor::State::kStopping &&
+          executor->tasks.size() == 0) {
+        executor->state = Executor::State::kStopped;
+        executor->stop.notify_all();
+      }
     }
+  }
+  std::cout<<"out\n";
 }
 
 void Executor::Stop(bool await) {
+  std::unique_lock<std::mutex> lock(mutex);
+  if (state == State::kRun) {
     state = State::kStopping;
-    empty_condition.notify_all();
-    if (await) {
-        std::unique_lock<std::mutex> lock(mutex);
-        stop.wait(lock, [&]() { return threads.size() == 0; });
-        while (!threads.empty())
-            threads.erase(threads.begin());
-    }
+  }
+  if (await && (threads > 0)) {
+    stop.wait(lock, [&]() { return threads + freeth == 0; });
+  } else {
     state = State::kStopped;
+  }
+  std::cout<<"stop\n";
 }
 
 void Executor::Start() {
-    state = State::kRun;
-    std::unique_lock<std::mutex> lock(mutex);
-    for (size_t i = 0; i < low_watermark; i++) {
-      std::thread t(&(perform), this);
-      threads.insert(std::move(std::make_pair(t.get_id(), std::move(t))));
-    }
-    freeth = 0;
+  state = State::kRun;
+  std::unique_lock<std::mutex> lock(mutex);
+  for (size_t i = 0; i < low_watermark; i++) {
+    std::thread t(&(perform), this);
+    t.detach();
+    freeth++;
+  }
 }
 
 } // namespace Concurrency
