a38cb294e1e6954cbf7ff482fa4340beabf398cd
Елисей <48096405+eles13@users.noreply.github.com>
2019-12-27 23:50:44 +0300

    hw6

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -7,13 +7,78 @@
 namespace Afina {
 namespace Coroutine {
 
-void Engine::Store(context &ctx) {}
+  void Engine::Store(context &ctx) {
+      char stack_end;
+      ctx.Hight = ctx.Low = StackBottom;
+      if (&stack_end > StackBottom) {
+          ctx.Hight = &stack_end;
+      }
+      else {
+          ctx.Low = &stack_end;
+      }
 
-void Engine::Restore(context &ctx) {}
+      char * &buffer = std::get<0>(ctx.Stack);
+      uint32_t &av_size = std::get<1>(ctx.Stack);
+      auto size = ctx.Hight - ctx.Low;
 
-void Engine::yield() {}
+      if (av_size < size) {
+          delete[] buffer;
+          buffer = new char[size];
+          av_size = size;
+      }
 
-void Engine::sched(void *routine_) {}
+      memcpy(buffer, ctx.Low, size);
+  }
+
+  void Engine::Restore(context &ctx) {
+      char stack_end;
+      if ((&stack_end >= ctx.Low) && (&stack_end <= ctx.Hight)) {
+          Restore(ctx);
+      }
+
+      char* &buffer = std::get<0>(ctx.Stack);
+      auto size = ctx.Hight - ctx.Low;
+
+      memcpy(ctx.Low, buffer, size);
+      longjmp(ctx.Environment, 1);
+  }
+
+
+  void Engine::yield() {
+      context * toyield = alive;
+
+      if (toyield && (toyield == cur_routine)) {
+          toyield = toyield->next;
+      }
+
+      if (toyield && toyield != cur_routine) {
+          Enter(*toyield);
+      }
+  }
+
+  void Engine::sched(void *routine_) {
+      if (cur_routine == routine_) {
+          return;
+      }
+
+      if (routine_) {
+          Enter(*(static_cast<context *>(routine_)));
+      } else {
+          yield();
+      }
+  }
+
+  void Engine::Enter(context& ctx) {
+    if (cur_routine && cur_routine != idle_ctx) {
+        if (setjmp(cur_routine->Environment) > 0) {
+            return;
+        }
+        Store(*cur_routine);
+    }
+
+    cur_routine = &ctx;
+    Restore(ctx);
+}
 
 } // namespace Coroutine
 } // namespace Afina
